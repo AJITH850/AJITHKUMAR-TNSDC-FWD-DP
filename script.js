d69a8679469d27b00e180930a13b985f3c9e292e
AJITH850 <kumarajith80420@gmail.com>
2025-08-29 12:06:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Show alert when form is submitted
+function showMessage() {
+    alert("Thanks! I will reply soon.");
+}
+
+// Set current year in footer
+document.getElementById("y").textContent = new Date().getFullYear();
